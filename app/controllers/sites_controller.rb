b4e24b71fc58c08d47c1da7336e88f208a7037dd
j-eaves <jdeaves@gmail.com>
2017-05-19 10:10:48 -0400

    Changed infoWindow function to accept an object as an argument (rather than strings) and to render specific object attributes, like images and descriptions. Eliminated the sourced of all console log errors.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -10,20 +10,24 @@ class SitesController < ApplicationController
 
 	def create
 		@address = params[:search_address]
-		p "*"*50
-		puts "ADDRESS FROM SITES CONTROLLER"
-		puts @address
-		p "*"*50
-		@coordinates = Geocoder.coordinates(@address)
-		@place = Geocoder.search(@address)
-		p "*"*50
-		puts "COORDINATES FROM SITES CONTROLLER"
-		#p @coordinates
-		#p @place
-		p "*"*50
-		@lat = @coordinates[0]
-		@lng = @coordinates[1]
-		render 'show.html.erb'
+		if @address.length > 0
+			p "*"*50
+			puts "ADDRESS FROM SITES CONTROLLER"
+			puts @address
+			p "*"*50
+			@coordinates = Geocoder.coordinates(@address)
+			@place = Geocoder.search(@address)
+			p "*"*50
+			puts "COORDINATES FROM SITES CONTROLLER"
+			#p @coordinates
+			#p @place
+			p "*"*50
+			@lat = @coordinates[0]
+			@lng = @coordinates[1]
+			render 'show.html.erb'
+		else
+			redirect_to '/'
+		end
 		#redirect_to '/sites/show'
 	end
 	def show
